a6c9a47c1f717fda0abb89f607f179073414363d
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-05-05 15:56:27 +0100

    Refactored if sellIn < 0 part to use specialItems

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,6 +12,8 @@ class Shop {
     this.items = items;
     this.specialItems = [ 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros' ]
   }
+  // Make a reduce item quality method -> only reduce if > 0 
+  // Make an increase item quality method -> increase if < 50
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       // If quality > 0 and is not a special item (use specialItems)
@@ -33,31 +35,24 @@ class Shop {
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      
+
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            // decrease quality of normal items if quality > 0
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+        if (this.specialItems.includes(this.items[i].name)) {
+          if (this.items[i].name == 'Aged Brie') {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
             }
-          // if it's a backstage pass and sellIn < 0 
-          } else {
+          }
+          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-      // if it's Aged Brie and sellin < 0 
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1;
           }
         }
       }
     }
-
-  
-
     return this.items;
   }
 
